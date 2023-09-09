6513979ca5217aa25470566944c408c47d92217b
Thiha Aung <thihaaung122122@gmail.com>
2023-09-10 00:07:45 +0630

    added forecast data

--- script.js
+++ script.js
@@ -23,6 +23,50 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class = "col-2">
+      <div class = "weatherr-forecast-date"> ${formatDay(
+        forecastDay.dt
+      )} </div> <div> 
+        <img src = "http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt = "" width = "42" />
+        </div> 
+        <diV class = "weather-forecast-temp">
+        <span class = "temp-max"> ${Math.round(forecastDay.temp.max)}°</span> 
+        <span class = "temp-min"> ${Math.round(forecastDay.temp.min)}° </span> 
+        </diV> </div> 
+        `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -43,9 +87,11 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
